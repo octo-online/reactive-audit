7815ab7307618cea6141a2c89e112efe8604d35f
github@prados.fr <github@prados.fr>
2014-09-15 08:40:17 +0200

    Add copyright

--- src/main/java/com/octo/reactive/audit/FileTools.java
+++ src/main/java/com/octo/reactive/audit/FileTools.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 OCTO Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.octo.reactive.audit;
 
 import java.io.*;
@@ -223,6 +239,7 @@ public final class FileTools
 		if (out.getClass().getName().equals("java.net.SocketOutputStream")) return NET_ERROR;
 		if (out instanceof FileOutputStream) return FILE_ERROR;
 		if (out.getClass().getName().startsWith("sun.net.www.")) return NET_ERROR;
+AuditReactive.config.debug("SOCKET OUTPUT="+out.getClass()); // FIXME
 		return NO_ERROR;
 	}
 

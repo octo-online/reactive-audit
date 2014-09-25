6cfd3bc3cb340e93b1db6bfc8d72bcde3594a5d1
yaciben <yab@octo.com>
2014-09-25 13:12:59 +0200

    Comments and minor changes to sample class

--- audit-reactive-integration/src/main/java/com/octo/reactive/sample/TestApp.java
+++ audit-reactive-integration/src/main/java/com/octo/reactive/sample/TestApp.java
@@ -1,23 +1,28 @@
-/**
+/*
+ * Copyright 2014 OCTO Technology
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.octo.reactive.sample;
 
-
 /**
- * @author Yacine
- *
+ * Sample application class for Reactive Audit tool tests.
  */
 public class TestApp {
     public static void main(String[] args) {
         System.out.println("Reactive test application is running!!");
 
-        startThread();
-
-        System.out.println("Reactive test application stopped.");
-    }
-
-    private static void startThread() {
         final Thread th = new Thread() {
             /* (non-Javadoc)
              * @see java.lang.Thread#run()
@@ -27,12 +32,13 @@ public class TestApp {
                 try {
                     Thread.sleep(200L);
                 } catch (final InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
         };
 
         th.start();
+
+        System.out.println("Reactive test application stopped.");
     }
 }
